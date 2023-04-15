047a9d049dd855d6c4e4984531108ba5fef53185
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-15 09:57:48 +0300

    Add main

---
+++ Stepik/Programming in Golang/1.  Введение в основы/1_1/main.go
@@ -0,0 +1,9 @@
+// 1.1 Напишите программу, которая выводит "I like Go!"
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+}
---
+++ Stepik/Programming in Golang/1.  Введение в основы/1_2/main.go
@@ -0,0 +1,9 @@
+// 1.2 Напишите программу, которая выведет "I like Go!" 3 раза.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!\nI like Go!\nI like Go!")
+}
