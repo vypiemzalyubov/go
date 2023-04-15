ac5ce14e493ecb3bd8a5f20014c90a19c1c73039
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-15 18:13:47 +0300

    Add 1_9 task

---
+++ Stepik/Programming in Golang/1.  Introduction to Basics/task/1_9/main.go
@@ -0,0 +1,22 @@
+// 1.9 Уберите лишние комментарии так, чтобы программа вывела число 100.
+//     package main
+//     import "fmt"
+//     func main(){
+//          // a:=44
+//          /*
+//          var a2 int = 10
+//         */
+//          a2 = a2 * 10
+//          fmt.Println(a2)
+//     }
+
+package main
+
+import "fmt"
+
+func main() {
+	// a:=44
+	var a2 int = 10
+	a2 = a2 * 10
+	fmt.Println(a2)
+}
