78c3e73446c660c823fcb957c234813bb92d6040
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-16 07:27:20 +0300

    Add 1_13 task

---
+++ Stepik/Programming in Golang/1.  Introduction to Basics/task/1_13/main.go
@@ -0,0 +1,22 @@
+// 1.13 Дано неотрицательное целое число. Найдите и выведите первую цифру числа.
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	switch {
+	case n < 10 && n >= 0:
+		fmt.Print(n)
+	case n >= 10 && n < 100:
+		fmt.Print(n / 10)
+	case n >= 100 && n < 1000:
+		fmt.Print(n / 100)
+	case n >= 1000 && n < 10000:
+		fmt.Print(n / 1000)
+	case n == 10000:
+		fmt.Print(n / 10000)
+	}
+}
