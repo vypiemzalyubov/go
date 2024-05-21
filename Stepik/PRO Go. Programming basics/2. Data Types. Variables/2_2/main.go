960446cbe0ae19b17ea62c4928ededbbe81c7287
dnikolaev <dnikolaev@tradingview.com>
2024-05-21 10:54:30 +0300

    Add module "2. Типы данных. Переменные"

---
+++ Stepik/PRO Go. Programming basics/2. Data Types. Variables/2_2/main.go
@@ -0,0 +1,18 @@
+// Измените предложенную программу, чтобы она выводила:
+// row1
+// row2
+// row3
+// row4
+//
+// В программе сделаны самые частые ошибки начинающего программиста.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("row1")
+	fmt.Println("row2")
+	fmt.Println("row3")
+	fmt.Println("row4")
+}
