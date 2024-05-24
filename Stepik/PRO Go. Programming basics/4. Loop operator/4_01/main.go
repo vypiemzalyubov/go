2649d904e7ea0f2f0fd59d4b3ffa0db5bf911e90
dnikolaev <dnikolaev@tradingview.com>
2024-05-24 16:20:35 +0300

    Add new tasks

---
+++ Stepik/PRO Go. Programming basics/4. Loop operator/4_01/main.go
@@ -0,0 +1,13 @@
+// Напишите программу, которая выводит "Hello, PRO Go!" (без кавычек) 19 раз, каждый на своей строке
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 19; i++ {
+		fmt.Println("Hello, PRO Go!")
+	}
+}
