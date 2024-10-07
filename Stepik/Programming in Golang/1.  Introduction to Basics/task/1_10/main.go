6fab40dd8920b7eee21447d9a72c2b0839489172
dnikolaev <dnikolaev@tradingview.com>
2024-10-07 15:41:59 +0300

    Add new task

--- Stepik/Programming in Golang/1.  Introduction to Basics/task/1_10/main.go
+++ Stepik/Programming in Golang/1.  Introduction to Basics/task/1_10/main.go
@@ -1,12 +1,12 @@
-// 1.10 Исправьте ошибку в программе ниже:
-//      package main
-//      import "fmt"
-//      func main(){
-//     		var a int = 8
-//     		const b int = 10
-//     		a = a + b
-//     		b = b + a
-//     		fmt.Println(a)
+// Исправьте ошибку в программе ниже:
+//   package main
+//   import "fmt"
+//   func main(){
+//      var a int = 8
+//     	const b int = 10
+//     	a = a + b
+//     	b = b + a
+//     	fmt.Println(a)
 // }
 
 package main
