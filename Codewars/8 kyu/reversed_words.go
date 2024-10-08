282610dd82fa4b45e8f5c2a3ca15b7bfdf64aef7
dnikolaev <dnikolaev@tradingview.com>
2024-10-08 14:49:05 +0300

    Update task

--- Codewars/8 kyu/reversed_words.go
+++ Codewars/8 kyu/reversed_words.go
@@ -31,9 +31,10 @@ package kata
 import "strings"
 
 func ReverseWords(str string) string {
-  words := strings.Split(str, " ")
-  for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
-        words[i], words[j] = words[j], words[i]
+  strSplit := strings.Split(str, " ")
+  var res []string
+  for i := len(strSplit)-1; i >= 0; i--{
+   res = append(res, strSplit[i]) 
   }
-  return strings.Join(words, " ")
+  return strings.Join(res, " ")
 }
