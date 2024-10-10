f9f561b711ad753704abb96993722baabc19fdc0
vypiemzalyubov <dima.rza90@mail.ru>
2024-10-10 19:16:53 +0300

    Add new task

---
+++ Codewars/6 kyu/ip_validation.go
@@ -0,0 +1,25 @@
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+//
+// Valid inputs examples:
+// Examples of valid inputs:
+// 1.2.3.4
+// 123.45.67.89
+//
+// Invalid input examples:
+// 1.2.3
+// 1.2.3.4.5
+// 123.456.78.90
+// 123.045.067.089
+//
+// Notes:
+// Leading zeros (e.g. 01.02.03.04) are considered invalid
+// Inputs are guaranteed to be a single string
+
+package kata
+
+import "regexp"
+
+func Is_valid_ip(ip string) bool {
+	matched, _ := regexp.MatchString(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$`, ip)
+	return matched
+}
